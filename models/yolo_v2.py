ab390b32d97880a4ae9c0349c6e159c4843e6d93
liaoyuhua <pluto4332@gmail.com>
2017-09-21 10:54:01 +0800

    build darknet yolo v2's forward part

--- models/yolo_v2.py
+++ models/yolo_v2.py
@@ -12,6 +12,26 @@ class EmptyModule(nn.Module):
 		super(EmptyModule,self).__init__()
 	def forward(self,x)
 		return x
+class yolo_v2_reorg(nn.Module):
+	def __init__(self,stride=2):
+		super(yolo_v2_reorg,self).__init__()
+		self.stride = stride
+	def forward(self,x):
+		stride = self.stride
+		assert(x.data.dim()==4)
+		B = x.data.size(0)
+		C = x.data.size(1)
+		H = x.data.size(2)
+		W = x.data.size(3)
+		assert(H % stride == 0)
+		assert(W % stride == 0)
+		ws = stride
+		hs = stride
+		x = x.view(B, C, H/hs, hs, W/ws, ws).transpose(3,4).contiguous()
+        x = x.view(B, C, H/hs*W/ws, hs*ws).transpose(2,3).contiguous()
+        x = x.view(B, C, hs*ws, H/hs, W/ws).transpose(1,2).contiguous()
+        x = x.view(B, hs*ws*C, H/hs, W/ws)
+        return x
 
 class yolo_v2(nn.Module):
 	def __init__(self):
@@ -27,6 +47,7 @@ class yolo_v2(nn.Module):
 		self.num_anchors  = len(self.anchors)/self.anchor_step
 		self.network_name = "yolo_v2"
 
+		self.layerInd_has_no_weigts = []
 	def create_model(self):
 		models = nn.ModuleList()
 
@@ -36,17 +57,17 @@ class yolo_v2(nn.Module):
 		conv0.add_moduel('bn0',nn.BatchNorm2d(32))
 		conv0.add_moduel('leaky0',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv0)
-		#max pool 0
+		#max pool 0 ind=1
 		models.append(nn.MaxPool2d(2,2))
-
+		self.layerInd_has_no_weigts.append(1)
 		#64
 		conv1.add_moduel('conv1',nn.Conv2d(32,64,3,1,1,bias=False))
 		conv1.add_moduel('bn1',nn.BatchNorm2d(64))
 		conv1.add_moduel('leaky1',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv1)
-		#max pool 1
+		#max pool 1 ind=3
 		models.append(nn.MaxPool2d(2,2))
-
+		self.layerInd_has_no_weigts.append(3)
         #128
 		conv2 = nn.Sequential()
 		conv2.add_moduel('conv2',nn.Conv2d(64,128,3,1,1,bias=False))
@@ -54,7 +75,7 @@ class yolo_v2(nn.Module):
 		conv2.add_moduel('leaky2',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv2)
 		conv3 = nn.Sequential()
-		conv3.add_moduel('conv3',nn.Conv2d(128,64,1,1,1,bias=False))
+		conv3.add_moduel('conv3',nn.Conv2d(128,64,1,1,0,bias=False))
 		conv3.add_moduel('bn3',nn.BatchNorm2d(64))
 		conv3.add_moduel('leaky3',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv3)
@@ -63,9 +84,9 @@ class yolo_v2(nn.Module):
 		conv4.add_moduel('bn4',nn.BatchNorm2d(128))
 		conv4.add_moduel('leaky4',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv4)
-		#max pool 2
+		#max pool 2  ind =7
 		models.append(nn.MaxPool2d(2,2))
-
+		self.layerInd_has_no_weigts.append(7)
 		#256
 		conv5 = nn.Sequential()
 		conv5.add_moduel('conv5',nn.Conv2d(128,256,3,1,1,bias=False))
@@ -73,7 +94,7 @@ class yolo_v2(nn.Module):
 		conv5.add_moduel('leaky5',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv5)
 		conv6 = nn.Sequential()
-		conv6.add_moduel('conv6',nn.Conv2d(256,128,1,1,1,bias=False))
+		conv6.add_moduel('conv6',nn.Conv2d(256,128,1,1,0,bias=False))
 		conv6.add_moduel('bn6',nn.BatchNorm2d(128))
 		conv6.add_moduel('leaky6',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv6)
@@ -82,9 +103,9 @@ class yolo_v2(nn.Module):
 		conv7.add_moduel('bn7',nn.BatchNorm2d(256))
 		conv7.add_moduel('leaky7',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv7)
-		#max pool 3 
+		#max pool 3 ind=11
 		models.append(nn.MaxPool2d(2,2))
-
+		self.layerInd_has_no_weigts.append(11)
 		#512
 		conv8 = nn.Sequential()
 		conv8.add_moduel('conv8',nn.Conv2d(256,512,3,1,1,bias=False))
@@ -92,7 +113,7 @@ class yolo_v2(nn.Module):
 		conv8.add_moduel('leaky8',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv8)
 		conv9 = nn.Sequential()
-		conv9.add_moduel('conv9',nn.Conv2d(512,256,1,1,1,bias=False))
+		conv9.add_moduel('conv9',nn.Conv2d(512,256,1,1,0,bias=False))
 		conv9.add_moduel('bn9',nn.BatchNorm2d(256))
 		conv9.add_moduel('leaky9',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv9)
@@ -102,18 +123,19 @@ class yolo_v2(nn.Module):
 		conv10.add_moduel('leaky10',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv10)
 		conv11 = nn.Sequential()
-		conv11.add_moduel('conv11',nn.Conv2d(512,256,1,1,1,bias=False))
+		conv11.add_moduel('conv11',nn.Conv2d(512,256,1,1,0,bias=False))
 		conv11.add_moduel('bn11',nn.BatchNorm2d(256))
 		conv11.add_moduel('leaky11',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv11)
+		#keep result ind=16
 		conv12 = nn.Sequential()
 		conv12.add_moduel('conv12',nn.Conv2d(256,512,3,1,1,bias=False))
 		conv12.add_moduel('bn12',nn.BatchNorm2d(512))
 		conv12.add_moduel('leaky12',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv12)
-		#max pool 4
+		#max pool 4 ind=17
 		models.append(nn.MaxPool2d(2,2))
-
+		self.layerInd_has_no_weigts.append(17)
 		#1024
 		conv13 = nn.Sequential()
 		conv13.add_moduel('conv13',nn.Conv2d(512,1024,3,1,1,bias=False))
@@ -121,7 +143,7 @@ class yolo_v2(nn.Module):
 		conv13.add_moduel('leaky13',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv13)
 		conv14 = nn.Sequential()
-		conv14.add_moduel('conv14',nn.Conv2d(1024,512,1,1,1,bias=False))
+		conv14.add_moduel('conv14',nn.Conv2d(1024,512,1,1,0,bias=False))
 		conv14.add_moduel('bn14',nn.BatchNorm2d(512))
 		conv14.add_moduel('leaky14',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv14)
@@ -131,7 +153,7 @@ class yolo_v2(nn.Module):
 		conv15.add_moduel('leaky15',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv15)
 		conv16 = nn.Sequential()
-		conv16.add_moduel('conv16',nn.Conv2d(1024,512,1,1,1,bias=False))
+		conv16.add_moduel('conv16',nn.Conv2d(1024,512,1,1,0,bias=False))
 		conv16.add_moduel('bn16',nn.BatchNorm2d(512))
 		conv16.add_moduel('leaky16',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv16)
@@ -147,46 +169,68 @@ class yolo_v2(nn.Module):
 		conv18.add_moduel('leaky18',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv18)
 
+		#keep result id=24
 		conv19 = nn.Sequential()
 		conv19.add_moduel('conv19',nn.Conv2d(1024,1024,3,1,1,bias=False))
 		conv19.add_moduel('bn19',nn.BatchNorm2d(1024))
 		conv19.add_moduel('leaky19',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv19)
 
-		#route
+		#route -9 id=25 
 		models.append(EmptyModule())
-
+		self.layerInd_has_no_weigts.append(25)
+		#conv     id=26
 		conv20 = nn.Sequential()
-		conv20.add_moduel('conv20',nn.Conv2d(1024,1024,3,1,1,bias=False))
-		conv20.add_moduel('bn20',nn.BatchNorm2d(1024))
+		conv20.add_moduel('conv20',nn.Conv2d(512,64,1,1,0,bias=False))
+		conv20.add_moduel('bn20',nn.BatchNorm2d(64))
 		conv20.add_moduel('leaky20',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv20)
-		#reorg
+		#reorg    id=27
 		models.append(yolo_v2_reorg(2))
-		#route
+		self.layerInd_has_no_weigts.append(27)
+		#route -1,-4 id=28
 		models.append(EmptyModule())
+		self.layerInd_has_no_weigts.append(28)
+		
+
+		#conv id =29
 		conv21 = nn.Sequential()
-		conv21.add_moduel('conv21',nn.Conv2d(1024,1024,3,1,1,bias=False))
+		conv21.add_moduel('conv21',nn.Conv2d(1280,1024,3,1,1,bias=False))
 		conv21.add_moduel('bn21',nn.BatchNorm2d(1024))
 		conv21.add_moduel('leaky21',nn.LeakyReLU(0.1,inplace=True))
 		models.append(conv21)
 
 		conv22 = nn.Sequential()
-		conv22.add_moduel('conv22',nn.Conv2d(1024,125,1,1,1,bias=False))
-		conv22.add_moduel('linear',nn.LeakyReLU(0.1,inplace=True))
+		conv22.add_moduel('conv22',nn.Conv2d(1024,125,1,1,0))
 		models.append(conv22)
 
+		return models
 
 	def forward(self,x):
-		for block in blocks:
+		outputs = dict()
+		for ind,model in enumerate(self.models):
+			#route 
+			if ind == 25:
+				input = outputs[ind-9]
+				x = model(input)
+			#route 
+			else if ind == 28:
+				input = torch.cat(outputs[ind-1],outputs[ind-4])
+				x = model(input)
+			else
+				x = model(x)
+				if id==16 or id==27 or id=24:
+					outputs[id]=x
+		return x
 
 	def load_conv(self,buf,start,conv_model):
 		num_w = conv_model.weight.numel()
 		num_b = conv_model.bias.numel()
-		conv_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_w]))
-		start = start + num_w
 		conv_model.bias.data.copy_(torch.from_numpy(buf[start:start+num_b]))
 		start = start +num_b
+		conv_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_w]).view(conv_model.weight.size()))
+		start = start + num_w
+
 		return start
 
 	def load_conv_bn(self,buf,start,conv_model,bn_model):
@@ -201,7 +245,7 @@ class yolo_v2(nn.Module):
 		start =start +num_b
 		bn_model.running_var.data.copy_(torch.from_numpy(buf[start:start+num_b]))
 		start =start +num_b
-		conv_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_w]))
+		conv_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_w]).view(conv_model.weight.size()))
 		start = start + num_w
 		return start
 
@@ -221,278 +265,39 @@ class yolo_v2(nn.Module):
 		print "weight file revision {} seen {}".format(revision,seen)
 		header = np.asarray([major,minor,revision,seen],dtype= np.int32)
 		buf = np.fromfile(fp,dtype = np.float32)
+		fp.close()
 		starte = 0
-		for model in models:
+		for ind,model in enumerate(self.models):
+			if ind not in self.layerInd_has_no_weigts:
+				if ind ==30:
+					start = load_conv_bn(buf, start, model[0], model[1])	
+				else:
+					start = load_conv(buf,start,model[0])
 
+	def convert2cpu(self,gpu_matrix):
+		return torch.FloatTensor(gpu_matrix.size()).copy_(gpu_matrix)
 
-	def create_blocks(self):
-		blocks = []
+	def save_conv(self,fp,conv_model):
+		if conv_model.bias.is_cuda:
+			convert2cpu(conv_model.bias.data).numpy().tofile(fp)
+			convert2cpu(conv_model.weight.data).numpy().tofile(fp)
+		else:
+			conv_model.bias.data.numpy().tofile(fp)
+			conv_model.weight.data.numpy().tofile(fp)
+	def save_conv_bn(self,fp,conv_model,bn_model):
+		if bn_model.bias.is_cuda:
+			convert2cpu(bn_model.bias.data).numpy().tofile(fp)
+			convert2cpu(bn_model.weight.data).numpy().tofile(fp)
+			convert2cpu(bn_model.running_mean.data).numpy().tofile(fp)
+			convert2cpu(bn_model.running_var.data).numpy().tofile(fp)
+			convert2cpu(conv_model.weight.data).numpy().tofile(fp)		
+		else:
+			bn_model.bias.data.numpy().tofile(fp)
+			bn_model.weight.data.numpy().tofile(fp)
+			bn_model.running_mean.data.numpy().tofile(fp)
+			bn_model.running_var.data.numpy().tofile(fp)
+			conv_model.weight.data.numpy().tofile(fp)		
 
-		block = edict()
-		block.type    		= "convolutional"
-		block.filters 		= 32
-		block.size    		= 3
-		block.pad     		= 1
-		block.stride  		= 1
-		block.activation 	= "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-		block = edict()
-		block.size         = 2
-		block.stride       = 2
-		blocks.append(block)
-
-		block.type    		= "convolutional"
-		block.filters 		= 64
-		block.size    		= 3
-		block.pad     		= 1
-		block.stride  		= 1
-		block.activation 	= "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-		block = edict()
-		block.size         = 2
-		block.stride       = 2
-		blocks.append(block)
-
-		block.type    		= "convolutional"
-		block.filters 		= 128
-		block.size    		= 3
-		block.pad     		= 1
-		block.stride  		= 1
-		block.activation 	= "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-		block.type    		= "convolutional"
-		block.filters 		= 64
-		block.size    		= 1
-		block.pad     		= 1
-		block.stride  		= 1
-		block.activation 	= "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-		block.type    		= "convolutional"
-		block.filters 		= 128
-		block.size    		= 3
-		block.pad     		= 1
-		block.stride  		= 1
-		block.activation 	= "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-		block = edict()
-		block.size         = 2
-		block.stride       = 2
-		blocks.append(block)
-
-		block.type    		= "convolutional"
-		block.filters 		= 256
-		block.size    		= 3
-		block.pad     		= 1
-		block.stride  		= 1
-		block.activation 	= "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-		block.type    		= "convolutional"
-		block.filters 		= 128
-		block.size    		= 1
-		block.pad     		= 1
-		block.stride  		= 1
-		block.activation 	= "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-		block.type    		= "convolutional"
-		block.filters 		= 256
-		block.size    		= 3
-		block.pad     		= 1
-		block.stride  		= 1
-		block.activation 	= "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-		block = edict()
-		block.size         = 2
-		block.stride       = 2
-		blocks.append(block)
-
-		block.type    		= "convolutional"
-		block.filters 		= 512
-		block.size    		= 3
-		block.pad     		= 1
-		block.stride  		= 1
-		block.activation 	= "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-		block.type    		= "convolutional"
-		block.filters 		= 256
-		block.size    		= 1
-		block.pad     		= 1
-		block.stride  		= 1
-		block.activation 	= "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-		block.type    		= "convolutional"
-		block.filters 		= 512
-		block.size    		= 3
-		block.pad     		= 1
-		block.stride  		= 1
-		block.activation 	= "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-		block.type    		= "convolutional"
-		block.filters 		= 256
-		block.size    		= 1
-		block.pad     		= 1
-		block.stride  		= 1
-		block.activation 	= "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-		block.type    		= "convolutional"
-		block.filters 		= 512
-		block.size    		= 3
-		block.pad     		= 1
-		block.stride  		= 1
-		block.activation 	= "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-
-		block = edict()
-		block.size         = 2
-		block.stride       = 2
-		blocks.append(block)
-
-
-		block.type    		= "convolutional"
-		block.filters 		= 1024
-		block.size    		= 3
-		block.pad     		= 1
-		block.stride  		= 1
-		block.activation 	= "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-		block.type    		= "convolutional"
-		block.filters 		= 512
-		block.size    		= 1
-		block.pad     		= 1
-		block.stride  		= 1
-		block.activation 	= "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-		block.type    		= "convolutional"
-		block.filters 		= 1024
-		block.size    		= 3
-		block.pad     		= 1
-		block.stride  		= 1
-		block.activation 	= "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-		block.type    		= "convolutional"
-		block.filters 		= 512
-		block.size    		= 1
-		block.pad     		= 1
-		block.stride  		= 1
-		block.activation 	= "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-		block.type    		= "convolutional"
-		block.filters 		= 1024
-		block.size    		= 3
-		block.pad     		= 1
-		block.stride  		= 1
-		block.activation 	= "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-##################################
-		block.type    		= "convolutional"
-		block.filters 		= 1024
-		block.size    		= 3
-		block.pad     		= 1
-		block.stride  		= 1
-		block.activation 	= "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-		block.type    		= "convolutional"
-		block.filters 		= 1024
-		block.size    		= 3
-		block.pad     		= 1
-		block.stride  		= 1
-		block.activation 	= "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-		block.type    		= "route"
-		block.layers 		= "-9"
-		blocks.append(block)
-
-		block.type    		  = "convolutional"
-		block.filters 		  = 64
-		block.size    		  = 1
-		block.pad     		  = 1
-		block.stride  		  = 1
-		block.activation 	  = "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-		block.type    		  = "route"
-		block.layers 		  = "-1,-4"
-		blocks.append(block)
-
-		block.type    		  = "convolutional"
-		block.filters 		  = 1024
-		block.size    		  = 3
-		block.pad     		  = 1
-		block.stride  		  = 1
-		block.activation 	  = "leaky"
-		block.batch_normalize = 1
-		blocks.append(block)
-
-		block.type    		  = "convolutional"
-		block.filters 		  = 125
-		block.size    		  = 1
-		block.pad     		  = 1
-		block.stride  		  = 1
-		block.activation 	  = "linear"
-		blocks.append(block)
-
-		block.type    		  = "region"
-		block.anchors 		  = "1.3221, 1.73145, 3.19275, 4.00944, 5.05587, 8.09892, 9.47112, 4.84053, 11.2364, 10.0071"
-		block.bias_match      = 1
-		block.classes     	  = 20
-		block.coords  		  = 4
-		block.num             = 5
-		block.softmax         = 1
-		block.jitter          = 0.3
-		block.rescore         = 1
-		block.object_scale    = 5
-		block.nnobject_scale  = 1
-		block.class_scale     = 1
-		block.coord_scale     = 1
-		block.absolute        = 1
-		block.thresh          = 0.6
-		block.random          = 1
-		
-		blocks.append(block)
-
-
-
-
-		return blocks
 
 
 
